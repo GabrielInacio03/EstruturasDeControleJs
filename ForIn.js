10e7aae048f474882126f657f41268e1a2568857
Gabriel Inacio <inaciogs03@gmail.com>
2022-01-03 10:26:54 -0300

    corrigido

--- ForIn.js
+++ ForIn.js
@@ -1,7 +1,7 @@
 //for in percorre os atributos de um objeto ou array
 const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
 
-for(i in notas){
+for(let i in notas){
     console.log(notas[i])
     //i = indice do array notas
 }
@@ -12,6 +12,6 @@ const pessoa = {
     peso: 69
 }
 
-for(atributo in pessoa){
+for(let atributo in pessoa){
     console.log(`${atributo} = ${pessoa[atributo]}`)
 }
