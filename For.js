a766884ea20497cc702e06e4690d0e2ce7efa3c2
Gabriel Inacio <inaciogs03@gmail.com>
2022-01-03 10:14:19 -0300

    estrutura de repeticao for

---
+++ For.js
@@ -0,0 +1,13 @@
+//uso do operador unário contador++
+for(let contador = 1; contador <= 10; contador++) {
+    console.log(`Contador => ${contador}.`)
+}
+
+for(let i = 1; i <= 10; i++){
+    console.log(`i = ${i}`)
+}
+
+const notas = [6.7, 7.4, 9, 10]
+for(i = 0; i < notas.length; i++){
+    console.log(`nota = ${notas[i]}`)
+}
